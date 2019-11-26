af1f9dbc03fa4c80a5b2b490f64f6047d84c4ce0
TobiasBenjaminYoungCGI <toby.young@cgi.com>
2019-11-26 14:08:39 +0000

    eh

--- src/main/java/app/PaddleChat.java
+++ src/main/java/app/PaddleChat.java
@@ -1,11 +1,20 @@
 package app;
 
+import models.Model;
+import models.Sql2oModel;
+import models.UserModel;
+import org.apache.log4j.BasicConfigurator;
 import org.eclipse.jetty.websocket.api.Session;
+import org.flywaydb.core.Flyway;
 import org.json.JSONObject;
+import org.sql2o.Sql2o;
+import org.sql2o.converters.UUIDConverter;
+import org.sql2o.quirks.PostgresQuirks;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static j2html.TagCreator.*;
@@ -17,6 +26,39 @@ public class PaddleChat {
     static int nextUserNumber = 1;
 
     public static void main(String[] args) {
+
+        BasicConfigurator.configure();
+
+        port(Main.getHerokuAssignedPort());
+
+        String dbHost = "localhost";
+        String dbName = "cgi_platypi";
+        String dbUser = null;
+        String dbPass = null;
+
+        if ( args.length != 0 ){
+            dbHost = args[0];
+            dbName = args[1];
+            dbUser = args[2];
+            dbPass = args[3];
+        }
+
+        Flyway flyway = Flyway.configure().dataSource("jdbc:postgresql://"+dbHost+":5432/"+dbName, dbUser,
+                dbPass).load();
+        flyway.migrate();
+
+        Sql2o sql2o = new Sql2o("jdbc:postgresql://" + dbHost + ":5432/" + dbName , dbUser,
+                dbPass, new PostgresQuirks() {
+            {
+                // make sure we use default UUID converter.
+                converters.put(UUID.class, new UUIDConverter());
+            }
+        });
+
+        Model model = new Sql2oModel(sql2o);
+        UserModel userModel = new Sql2oModel(sql2o);
+
+
         staticFileLocation("/public");
         webSocket("/PaddleChat", PaddleChatWebSocketHandler.class);
         init();
