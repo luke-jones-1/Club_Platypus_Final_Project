aca54019cdf522db4d539c4e6844abe77b977f7b
Luke Jones <lukejonesice@yahoo.co.uk>
2019-11-27 15:32:07 +0000

    updating chatRunner.js

--- src/main/resources/public/chatRunner.js
+++ src/main/resources/public/chatRunner.js
@@ -2,7 +2,7 @@
 //console.log(location.port);
 
 // creates a web socket on page load
-var webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/chat");
+var webSocket = new WebSocket("wss://" + location.hostname + ":" + location.port + "/chat");
 
 // to update the chat when new message created
 webSocket.onmessage = function (msg) { updateChat(msg); };
