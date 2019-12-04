141e4cb82de97eff27e737bb10836108e8a09643
albie-b <55197619+albie-b@users.noreply.github.com>
2019-12-04 16:03:08 +0000

    emoji keyboard works

--- src/main/resources/public/emojiJquery.js
+++ src/main/resources/public/emojiJquery.js
@@ -1,11 +1,14 @@
 //emoji keyboard
-// jquery
-$(document).ready(function(){
-  $(".emojiFrame").click(function(){
-    // sets target as message box
-        $("#message").append($(this).text());
-//    var target = $("#message");
-//    // adds the emoji  to end of text box
-//    target.text(target.text() + $(this).text());
+  // jquery
+  $(document).ready(function(){
+    $(".emojiFrame").click(function(){
+      // sets target as message box
+//          $("#message").append($(this).text());
+      var target = $("#message");
+  //    // adds the emoji  to end of text box
+  //    target.text(target.text() + $(this).text());
+      var temp = target.val()
+      $('#message').val(temp + $(this).text());
+    })
   })
-})
+
