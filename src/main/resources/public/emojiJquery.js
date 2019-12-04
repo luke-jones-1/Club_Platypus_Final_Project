8535acaf95900a4a930ff2ef41374d44bbaa9ee7
albie-b <55197619+albie-b@users.noreply.github.com>
2019-12-04 14:35:31 +0000

    sort of working

---
+++ src/main/resources/public/emojiJquery.js
@@ -0,0 +1,11 @@
+//emoji keyboard
+// jquery
+$(document).ready(function(){
+  $(".emojiFrame").click(function(){
+    // sets target as message box
+        $("#message").append($(this).text());
+//    var target = $("#message");
+//    // adds the emoji  to end of text box
+//    target.text(target.text() + $(this).text());
+  })
+})
